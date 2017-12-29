13ddb79f2acad1f44c2a3d51df0db48c20e77a2d
Brandon Hillesheim <zosoele@gmail.com>
2017-12-29 01:29:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ function happyHolidays(){
   return "Happy holidays!";
 }
 function happyHolidaysTo(name){
-  return `Happy Holidays, ${name}!`;
+  return `Happy holidays, ${name}!`;
 }
-function happyHolidaysTo(holiday, name){
+function happyHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
 function holidayCountdown(holiday, days){
